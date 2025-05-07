c1b57e6aafba211685f79109f2a8fd4b53a8a1b0
Robbys <robbys@Robbyss-MacBook-Pro.local>
2025-05-07 11:53:55 +0800

    web statis dengan laravel

--- php-2/uhuy.py
+++
@@ -1,7 +0,0 @@
-# axis
-duitGabung = np.array([duitA, duitB]) # menggabung array duitA & duitB
-print(duitGabung)
-print("\n")
-print(duitGabung.sum(axis=0)) # jumlah per kolom
-print("\n")
-print(duitGabung.sum(axis=1)) # jumlah per baris
